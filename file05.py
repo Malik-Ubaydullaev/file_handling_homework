8c77084f1744ca2194b4993da4a037cd92eac7ea
Your Name <ubaydullayev@samtuit.uz>
2022-01-20 20:41:11 +0500

    5-misol

--- file05.py
+++ file05.py
@@ -6,5 +6,16 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    idx = 0
+    number_count = 0
+    string_count = 0
+    while idx < len(data):
+        if data[idx].isdigit():
+            number_count += 1
+        else:
+            string_count += 1
+        idx += 1
+    list2 = [number_count, string_count]
+    return list2
+        
 # Read data from file
