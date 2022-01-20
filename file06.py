65c5454564ece6d5d41d5b1367369f5f7d7708a5
Your Name <ubaydullayev@samtuit.uz>
2022-01-20 21:22:48 +0500

    6-misol

--- file06.py
+++ file06.py
@@ -6,5 +6,16 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    list1 = data.split()
+    print(list1)
+    list2 = []
+    idx = 0
+    while idx < len(list1):
+        print(list1[idx])
+        list2.append(len(list1[idx]))
+        #list1.pop()
+        idx += 1
+    
+    return list2
     
 # Read data from file
