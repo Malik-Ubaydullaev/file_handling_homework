3ffb9de0079d2af0396c5c52466bcd34f6d8e4af
Your Name <ubaydullayev@samtuit.uz>
2022-01-20 20:23:43 +0500

    4-misol

--- file04.py
+++ file04.py
@@ -6,5 +6,13 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    list1 = data.split(',')
+    list2 = []
+    idx = 0
+    while idx < len(list1):
+        if not list1[idx].isdigit():
+            list2 += list1[idx]
+        idx += 1
+    list2 = list(map(str,list2))
+    return list2
 # Read data from file
