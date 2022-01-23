db921fb8f972c6294f3fe5296aacf0a86d4697d8
Your Name <ubaydullayev@samtuit.uz>
2022-01-23 20:20:56 +0500

    3-d task

--- file03.py
+++ file03.py
@@ -6,13 +6,13 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    list1 = data.split('\n')
+    list1 = data.split()
     list2 = []
     idx = 0
     while idx < len(list1):
         if list1[idx].isdigit():
             list2 += list1[idx]
         idx += 1
-    list2 = list(map(str,list2))
+    #list2 = list(map(str,list2))
     return list2
 # Read data from file
